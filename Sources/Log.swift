65a29665e89fe03d170dc6bf49f1dbd2bcd8db33
Stephen Lynn <stephen.g.lynn@gmail.com>
2018-12-05 21:55:28 -0700

    Update logging to include file/line number

--- Sources/Log.swift
+++ Sources/Log.swift
@@ -5,41 +5,41 @@ public enum LogLevel: Int {
 }
 
 public protocol Logger {
-  func debug(_ message: String)
-  func verbose(_ message: String)
-  func error(_ message: String)
-  func warn(_ message: String)
-  func info(_ message: String)
-  func exception(_ error: NSError)
+  func debug(_ message: String, file: StaticString, line: UInt)
+  func verbose(_ message: String, file: StaticString, line: UInt)
+  func error(_ message: String, file: StaticString, line: UInt)
+  func warn(_ message: String, file: StaticString, line: UInt)
+  func info(_ message: String, file: StaticString, line: UInt)
+  func exception(_ error: NSError, file: StaticString, line: UInt)
 }
 
 public class Log: Logger {
   public static var logger: Logger = Log()
   public static var logLevel = LogLevel.debug
   
-  public class func debug(_ message: String) {
-    guard shouldLog(requestLevel: .verbose) else { return }
-    logger.debug(message)
+  public class func debug(_ message: String, file: StaticString = #file, line: UInt = #line) {
+    guard shouldLog(requestLevel: .debug) else { return }
+    logger.debug(message, file: file, line: line)
   }
-  public class func verbose(_ message: String) {
+  public class func verbose(_ message: String, file: StaticString = #file, line: UInt = #line) {
     guard shouldLog(requestLevel: .verbose) else { return }
-    logger.verbose(message)
+    logger.verbose(message, file: file, line: line)
   }
-  public class func error(_ message: String) {
+  public class func error(_ message: String, file: StaticString = #file, line: UInt = #line) {
     guard shouldLog(requestLevel: .error) else { return }
-    logger.error(message)
+    logger.error(message, file: file, line: line)
   }
-  public class func warn(_ message: String) {
+  public class func warn(_ message: String, file: StaticString = #file, line: UInt = #line) {
     guard shouldLog(requestLevel: .warn) else { return }
-    logger.warn(message)
+    logger.warn(message, file: file, line: line)
   }
-  public class func info(_ message: String) {
+  public class func info(_ message: String, file: StaticString = #file, line: UInt = #line) {
     guard shouldLog(requestLevel: .info) else { return }
-    logger.info(message)
+    logger.info(message, file: file, line: line)
   }
-  public class func exception(_ error: NSError) {
+  public class func exception(_ error: NSError, file: StaticString = #file, line: UInt = #line) {
     guard shouldLog(requestLevel: .exception) else { return }
-    logger.exception(error)
+    logger.exception(error, file: file, line: line)
   }
   
   private class func shouldLog(requestLevel: LogLevel) -> Bool {
@@ -47,22 +47,22 @@ public class Log: Logger {
     return true
   }
   
-  public func debug(_ message: String) {
-    print("D: \(message)")
+  public func debug(_ message: String, file: StaticString, line: UInt) {
+    print("D:\(file):\(line): \(message)")
   }
-  public func verbose(_ message: String) {
-    debug(message)
+  public func verbose(_ message: String, file: StaticString, line: UInt) {
+    print("V:\(file):\(line): \(message)")
   }
-  public func error(_ message: String) {
-    print("E: \(message)")
+  public func error(_ message: String, file: StaticString, line: UInt) {
+    print("E:\(file):\(line): \(message)")
   }
-  public func warn(_ message: String) {
-    print("W: \(message)")
+  public func warn(_ message: String, file: StaticString, line: UInt) {
+    print("W:\(file):\(line): \(message)")
   }
-  public func info(_ message: String) {
-    print("I: \(message)")
+  public func info(_ message: String, file: StaticString, line: UInt) {
+    print("I:\(file):\(line): \(message)")
   }
-  public func exception(_ error: NSError) {
-    self.error(error.localizedDescription)
+  public func exception(_ error: NSError, file: StaticString, line: UInt) {
+    self.error(error.localizedDescription, file: file, line: line)
   }
 }
--- sModel.podspec
+++ sModel.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "sModel"
-  s.version      = "1.0.13"
+  s.version      = "1.0.14"
   s.summary      = "sModel is a lightweight object mapper for sqlite."
 
   s.description  = <<-DESC
